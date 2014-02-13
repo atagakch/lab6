ad7a107616129595bedaffa28ca58047b2fcb4b3
atagakch <atagakch@gmail.com>
2014-02-13 10:50:54 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,19 +14,40 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+var projectHTML;
+var object;
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
+	object = $(this).next();
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("http://localhost:3000/project/" + idNumber);
+	//$(this).next().html("FOO");
+	$.get("http://localhost:3000/project/" + idNumber, cbfnct);
+	//$(this).next().html(projectHTML);
+}
+
+
+function cbfnct(result){
+	console.log(result);
+	projectHTML = result['summary'];
+	projectHTML += '<img class="detailsImage" src='+result['image'] + '>';
+	object.html(projectHTML);
+}
+
+function colorfn(result){
+	console.log(result);
+	projectHTML = result['summary'];
+	projectHTML += '<img class="detailsImage" src='+result['image'] + '>';
+	object.html(projectHTML);
 }
 
 /*
@@ -35,4 +56,5 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("http://localhost:3000/palatte", colorfn);
 }
